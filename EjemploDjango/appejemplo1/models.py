ebda074aaf9b8fe8bfddcb8d30d8c7b507763769
REVEGEZSX <stivenrafael@hotmail.com>
2019-08-15 15:29:59 -0500

    se agregaron clases

--- EjemploDjango/appejemplo1/models.py
+++ EjemploDjango/appejemplo1/models.py
@@ -4,3 +4,15 @@ from django.db import models
 class Libro(models.Model):
     name = models.CharField(max_length = 20)
     author = models.CharField(max_length = 20)
+    campoActivo = models.BooleanField
+class estante(models.Model):
+    fila = models.IntegerField
+    columna = models.IntegerField
+    campoActivo = models.BooleanField
+class prestamo(models.Model):
+    fechaPrestamo = models.DateTimeField
+    fechaEntrega = models.DateField
+    campoActivo = models.BooleanField
+class cliente(models.Model):
+    nombreCliente = models.CharField(max_length = 20)
+    apellidoCliente = models.CharField(max_length = 20)
